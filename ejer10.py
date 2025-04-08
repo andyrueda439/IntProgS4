c37eca4982acf1d1d2efe97d137b2b61e30b3078
andyrueda439 <ajrueda@uamv.edu.ni>
2025-04-08 18:04:45 -0500

    Add files via upload

---
+++ ejer10.py
@@ -0,0 +1,23 @@
+#Declarar nombre de variables con la convencion indicada
+
+# 1. Nombre completo del estudiante – camelCase
+nombreCompletoDelEstudiante = "Lucía Gómez"
+
+# 2. Número de matrícula – snake_case
+numero_de_matricula = "A2023101"
+
+# 3. Total de ventas del mes – PascalCase
+TotalDeVentasDelMes = 32000
+
+# 4. Límite máximo de intentos – SCREAMING_SNAKE_CASE
+LIMITE_MAXIMO_DE_INTENTOS = 5
+
+# 5. Cantidad de productos – camelCase
+cantidadDeProductos = 120
+
+print("\nEjercicio 3:")
+print(nombreCompletoDelEstudiante)
+print(numero_de_matricula)
+print(TotalDeVentasDelMes)
+print(LIMITE_MAXIMO_DE_INTENTOS)
+print(cantidadDeProductos)
